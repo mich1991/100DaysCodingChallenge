7c2c4e404c067193d117edcfb002c16c7e3c2e0c
Michal Wozniak <wozniak.michal91@gmail.com>
2020-09-21 15:41:58 +0100

    deleted console log

--- 19.Trump_Quote_Generator/app.js
+++ 19.Trump_Quote_Generator/app.js
@@ -4,7 +4,6 @@ const quote = document.querySelector('.quote')
 
 getQuote = async () => {
     let response = await axios('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
-    console.log(response.data.message)
     let quote = response.data.message
     return quote
 }
